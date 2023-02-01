9e961378f596939380433f9996512b7458d78597
octavioamu <octavioamuchastegui@gmail.com>
2023-01-31 23:15:20 -0300

    fix: deploy yaml

--- pages/api/doc.ts
+++ pages/api/doc.ts
@@ -1,5 +1,9 @@
 import { SWAGGER_DESCRIPTION } from "@/config/general";
 import { withSwagger } from "next-swagger-doc";
 
-const swaggerHandler = withSwagger(SWAGGER_DESCRIPTION);
+const swaggerHandler = withSwagger({
+  ...SWAGGER_DESCRIPTION,
+  apiFolder: "pages/api",
+  schemaFolders: ["openapi"],
+});
 export default swaggerHandler();
