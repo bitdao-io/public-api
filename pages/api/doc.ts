b7b6de483ae11caf96cec0ed7ae5e186b5390dc1
octavioamu <octavioamuchastegui@gmail.com>
2023-02-01 11:45:35 -0300

    fix: move doc to endpoint gen

--- pages/api/doc.ts
+++ pages/api/doc.ts
@@ -1,5 +1,35 @@
 import { SWAGGER_DESCRIPTION } from "@/config/general";
-import { withSwagger } from "next-swagger-doc";
+import { NextApiRequest, NextApiResponse } from "next";
 
-const swaggerHandler = withSwagger(SWAGGER_DESCRIPTION);
-export default swaggerHandler();
+import { createSwaggerSpec } from "next-swagger-doc";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const swaggerHandler = createSwaggerSpec(SWAGGER_DESCRIPTION);
+    console.log(swaggerHandler);
+    const results = swaggerHandler;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    res.json({
+      success: true,
+      statusCode: 200,
+      results: results,
+    });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ success: false, statusCode: 500, message: error?.message });
+  }
+};
+export default handler;
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   const spec: Record<string, any> = createSwaggerSpec(SWAGGER_DESCRIPTION);
+
+//   return {
+//     props: {
+//       spec,
+//     },
+//   };
+// };
+
+// export default swaggerHandler();
