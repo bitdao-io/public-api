64d4691bedc98c7523eededeec10f7e20bb153b3
octavioamu <octavioamuchastegui@gmail.com>
2023-01-31 21:49:33 -0300

    fix: schemas being removed

--- next.config.js
+++ next.config.js
@@ -4,14 +4,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  swcMinify: true,
+  // swcMinify: true,
 
-  // compiler: {
-  //   //   // ssr and displayName are configured by default
-  //   styledComponents: true,
-  //   removeConsole: true,
-  //   swcMinify: true,
-  // },
+  compiler: {
+    //   // ssr and displayName are configured by default
+    styledComponents: true,
+    removeConsole: true,
+    swcMinify: true,
+  },
 };
 
 module.exports = nextConfig;
