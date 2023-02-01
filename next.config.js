0a222206597f42253d7906e94ff94e7994cb16ff
octavioamu <octavioamuchastegui@gmail.com>
2023-02-01 10:50:31 -0300

    fix: prebuild

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     //   // ssr and displayName are configured by default
     styledComponents: true,
     removeConsole: true,
-    swcMinify: true,
+    // swcMinify: true,
   },
 };
 
